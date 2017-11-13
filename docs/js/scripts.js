22dbeed7a1f1a16bb9161fbbd0d951c87c0a8337
Nikita <nikita.kruchinkin@gmail.com>
2017-11-13 15:52:11 +0300

    Complete main

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -6,5 +6,10 @@ $(function (){
     $(this).toggleClass('select--active');
   });
 
+  $('.navbar__toggler').click(function(e){
+    var target = $(this).data('target');
+    $('#'+target).toggleClass('nav--active');
+  });
+
   VK.Widgets.Group("vk_groups", {mode: 1, width: "185", height: "170", color1: "FFFFFF", color2: "2B587A", color3: "5B7FA6"}, 21248567);
 });
