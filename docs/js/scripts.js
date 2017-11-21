ff4cb81ae52d21bbce822082be8485dc38fb88fd
Nikita <nikita.kruchinkin@gmail.com>
2017-11-21 13:36:44 +0300

    Two more pages

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -11,5 +11,5 @@ $(function (){
     $('#'+target).toggleClass('nav--active');
   });
 
-  VK.Widgets.Group("vk_groups", {mode: 1, width: "185", height: "170", color1: "FFFFFF", color2: "2B587A", color3: "5B7FA6"}, 21248567);
+  VK.Widgets.Group("vk_groups", {mode: 1, width: "173", height: "170", color1: "FFFFFF", color2: "2B587A", color3: "5B7FA6"}, 21248567);
 });
